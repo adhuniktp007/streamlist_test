c56e4509a85ee8bfef70ec1c1728293cf95e0075
Adhunik TP <154965752+adhuniktp007@users.noreply.github.com>
2026-07-03 10:12:59 +0530

    Fix capitalization and formatting in app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 #take a user input\
-name=st.text_input("enter your name")
+name=st.text_input("Enter your name")
 
-st.title("take the input")
+st.title("Take the input")
 
-if st.button("submit"):
+if st.button("Submit"):
   st.write(f"print the name: {name}")
